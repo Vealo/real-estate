92b260eb1e566f15f0a2e375c240b5a50b0a1752
Denis Karmalak <vealo@yandex.ru>
2024-07-24 17:10:01 +0300

    Add drf-spectacula

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templated'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- realty/views.py
+++ realty/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse('<h2>Главная</h2>')
-
-
-def about(request):
-    return HttpResponse('<h2>О сайте</h2>')
-
-
-def contact(request):
-    return HttpResponse('<h2>Контакты</h2>')
+    data = { "header" : "Hello Django", "message" : "Welcome to Python!"}
+    return render(request, "realty/index.html", context=data)
