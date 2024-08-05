d29039ae6a97debf68d899aa3ade4778044224a9
Denis Karmalak <vealo@yandex.ru>
2024-08-05 17:55:25 +0300

    Add/refactoring models: Building, Section, Floor

--- realty/models.py
+++ realty/models.py
@@ -1,43 +1,141 @@
 from django.db import models
-from django.utils import timezone
 import uuid
 
 #Справочники
-#Тип дома
-class TepyBuildings(models.Model):
+class BuildingType(models.Model):
+    """Справочник типов строений"""
     type = models.CharField(max_length=50,help_text="Наименование")
     description = models.CharField(max_length=250, help_text="Описание")
-# Страны
-class Сountries(models.Model):
-    full_name = models.CharField(max_length=100, help_text="Название страны")
+
+
+class Сountrie(models.Model):
+    """Справочник стран"""
+    name = models.CharField(max_length=100, help_text="Название страны")
     abbreviations = models.CharField(max_length=4, help_text="Абривиатура")
 
-#Код субъекта
-class CodeRegions(models.Model):
+
+
+class RegionCode(models.Model):
+    "Справочник код субъектов"
     code = models.CharField(max_length=4, help_text="Код региона")
-#Субъект
-class Regions(models.Model):
-    full_name = models.CharField(max_length=100, help_text="Название субьекта")
+
+
+class Region(models.Model):
+    """Справочник субъектов"""
+    name = models.CharField(max_length=100, help_text="Название субъекта")
     abbreviations = models.CharField(max_length=4, help_text="Абривиатура")
-    code = models.ForeignKey(CodeRegions, help_text="Код региона")
+    code = models.ForeignKey(RegionCode, help_text="Код региона")
+
+
+class Locality(models.Model):
+    """Справочник начеленных пунктов    """
+    name = models.CharField(max_length=250, help_text="Назвение населенного пункта")
+
+
+class StreetType(models.Model):
+    """Справочник типов дорог в населенном пункте. Пример: улица."""
+    name = models.CharField(max_length=100, help_text="Тип дороги (улицы) в городе")
+    abbreviations = models.CharField(max_length=6, help_text="Абривиатура названия")
+
+
+class BuildingType(models.Model):
+    """Тип строения."""
+    name = models.CharField(max_length=150, help_text="Наименование типа строения")
+    description = models.TextField(help_text="Описание строения")
+
+
+class SectionType(models.Model):
+    """Справочник типов секций"""
+    name = models.CharField(max_length=50, help_text="Наименование секции")
+
+
+class EntranceType(models.Model):
+    """Справочник типов входов"""
+    name = models.CharField(max_length=50, help_text="Тип входа (парадная / подъезд)")
+
+
+class Entrance(models.Model):
+    """Справочник входов"""
+    entrance_type = models.OneToOneField(EntranceType, help_text="Тип входа")
+    numbers = models.CharField(max_length=10, help_text="Номер входа")
+    description = models.TextField(help_text="Описание")
+
+
+class EntranceSpecial(models.Model):
+    """Нестандартных справочников входов"""
+    name = models.CharField(max_length=150, help_text="Наименование входа")
+    description = models.TextField(help_text="Описание")
+
+
+class Elevator(models.Model):
+    """Справочник лифтов"""
+    name = models.CharField(max_length=150, help_text="Название лифта")
+    description = models.TextField(help_text="Описание")
+
+
+class Ladder(models.Model):
+    """Справочник лесница"""
+    name = models.CharField(max_length=150, help_text="Название лесница")
+    description = models.TextField(help_text="Описание")
+
+#Миксины
+class BaseTime(models.Model):
+    "Класс миксин - создание, изменение"
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
 
 #Модели
-class Buildings(models.Model):
-    '''
+class Building(models.Model, BaseTime):
+    """
     Здание
     - Адрес
     - Тип строения
     - Этажность
     - Площадь
     - Описание
-    '''
+    """
     #Adress
+    id = models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True)
     index = models.CharField(max_length=10, help_text="Индекс")
-    country = models.OneToOneField(Сountries, on_delete=models.SET_NULL, help_text="Страна")
-    region = models.OneToOneField(Regions, on_delete=models.SET_NULL, help_text="Субъект")
+    country = models.OneToOneField(Сountrie, on_delete=models.SET_NULL, help_text="Страна")
+    region = models.OneToOneField(Region, on_delete=models.SET_NULL, help_text="Субъект")
+    locality = models.OneToOneField(Locality, on_delete=models.SET_NULL, help_text="Начеленный пункт")
+    street_type = models.OneToOneField(StreetType, on_delete=models.SET_NULL, help_text="Тип дороги")
+    street = models.CharField(max_length=250, help_text="Название дороги (улицы)")
+    building_type = models.OneToOneField(BuildingType, on_delete=models.SET_NULL, help_text="Тип строения")
+    building_number = models.CharField(max_length=10, help_text="Номер сроения")
 
     #Common
-    type = models.OneToOneField(TepyBuildings, on_delete=models.SET_NULL, help_text="Тип дома")
+    type = models.OneToOneField(BuildingType, on_delete=models.SET_NULL, help_text="Тип дома")
     total_space = models.IntegerField(help_text="Этажность")
     total_square = models.FloatField(help_text="Общая площадь")
     description = models.CharField(help_text="Описание")
+
+
+    class Meta:
+        ordering = ["-created"]
+
+class Section(models.Model, BaseTime):
+    """Секция дома."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    number = models.IntegerField(help_text="Номер секции")
+    section_type = models.OneToOneField(SectionType, on_delete=models.SET_NULL, help_text="Тип секции")
+    entrance_special = models.ForeignKey(EntranceSpecial, help="")
+    description = models.TextField(help_text="Описание секции")
+    building = models.ForeignKey(Building, help_text="Дом")
+    max_number_of_floors = models.IntegerField(help_text="Этажность")
+
+    class Meta:
+        ordering = ['number']
+
+class Floor(models.Model, BaseTime):
+    """Этаж секции"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    number = models.IntegerField(help_text="Номер этажа")
+    ladder = models.ForeignKey(Ladder, help_text="Лифт", on_delete=models.SET_NULL(), default=None)
+    elevator = models.ForeignKey(Elevator, help_text="Лесница", on_delete=models.SET_NULL(), default=None)
+    section = models.ForeignKey(Section, help_text="", on_delete=models.SET_NULL(), default=None)
+
+    class Meta:
+        ordering = ['number']
